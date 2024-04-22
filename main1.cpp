3e3393cbcfccdee8106d7fda8a3c99e47c001e44
manishghule88 <manishghule26@gmail.com>
2024-04-22 19:03:27 +0530

    First Commit

---
+++ main1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int Ans = 0;
+    int iNo1 = 0;
+    int iNo2 = 0;
+
+    cin >> iNo1;
+    cin >> iNo2;
+
+    Ans = iNo1 + iNo2;
+    cout << Ans;
+    
+    return 0;
+}
